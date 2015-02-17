5c97775f9234552cc7f392aa4e40fc7da3e25c9d
Christopher Small <csmall@fhcrc.org>
2015-02-16 16:48:00 -0800

    Cleaning up project.clj a bit

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject posterior-subset "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple Clojure/JVM CLI for subsetting BEAST posterior files (logfiles and treefiles)"
+  :url "https://github.com/metasoarous/posterior-subset"
   :main posterior-subset.core
   :aot [posterior-subset.core]
+  :bin {:name "posterior-subset"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-bin "0.3.4"]]
-  :bin {:name "posterior-subset"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]])
